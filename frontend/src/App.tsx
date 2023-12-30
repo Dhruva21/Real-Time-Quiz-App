879fb059f079d64ca7444b74aaed583bf0150a6c
dhruvaindavarapu <idhruva@horizon.csueastbay.edu>
2023-12-29 19:58:42 -0800

    Added main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+          <Route path="/" element={<Admin />} />
           <Route path="admin" element={<Admin />} />
           <Route path="user" element={<User />} />
       </Routes>
